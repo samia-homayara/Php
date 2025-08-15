a1d052d69889d1aad784a417d6a01db6a127510d
samia-homayara <samiahomayara551@gmail.com>
2025-08-15 22:05:41 +0600

    largest_number.php

--- Largest_Number/largest_number.php
+++ Largest_Number/largest_number.php
@@ -1,1 +1,18 @@
+<?php
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
 
+$largest = $num1;
+
+if ($num2 > $largest) {
+    $largest = $num2;
+}
+
+if ($num3 > $largest) {
+    $largest = $num3;
+}
+
+echo "The largest number is: " . $largest;
+
+?>
